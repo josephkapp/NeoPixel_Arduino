5b635bbab68466041885b61a674bf642e467d784
Joseph Kapp <joseph.a.kapp@gmail.com>
2019-04-09 23:16:18 -0400

    fix untracked files

--- Debug/Adafruit_NeoPixel/esp8266.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Joseph\AppData\Local\Temp\VMBuilds\RGB_Neopixel\uno\Debug\Adafruit_NeoPixel\esp8266.c.o: \
- C:\Users\Joseph\Documents\Arduino\libraries\Adafruit_NeoPixel\esp8266.c
--- Release/Adafruit_NeoPixel/esp8266.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Joseph\AppData\Local\Temp\VMBuilds\RGB\uno\Release\Adafruit_NeoPixel\esp8266.c.o: \
- C:\Users\Joseph\Documents\Arduino\libraries\Adafruit_NeoPixel\esp8266.c
